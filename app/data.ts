1a8ab1a6d28d61b4a27da69747bfb82bb2426f8c
Naoto Kubota <kubogen3@gmail.com>
2025-06-02 14:11:48 +0900

    feat: add product image assets

--- app/data.ts
+++ app/data.ts
@@ -3,6 +3,7 @@ type Project = {
   description: string
   link: string
   video: string
+  image: string
   id: string
 }
 
@@ -42,15 +43,9 @@ export const PROJECTS: Project[] = [
     link: 'https://hotlist.social/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    image:
+      '/hotlist_logo.jpg',
     id: 'project1',
-  },
-  {
-    name: '？？？？',
-    description: '新しいプロダクトを開発中です。お楽しみに！',
-    link: 'https://hotlist.social/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
   },
 ]
 
@@ -61,12 +56,12 @@ export const SERVICES: Service[] = [
     link: '/consulting',
     id: 'service1',
   },
-  {
-    name: 'AI駆動開発サービス',
-    description: '最新のAIツールを活用して効率的かつ高品質なWebサイトを制作します。',
-    link: '/ai-driven-development',
-    id: 'service2',
-  },
+  // {
+  //   name: 'AI駆動開発サービス',
+  //   description: '最新のAIツールを活用して効率的かつ高品質なWebサイトを制作します。',
+  //   link: '/ai-driven-development',
+  //   id: 'service2',
+  // },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
